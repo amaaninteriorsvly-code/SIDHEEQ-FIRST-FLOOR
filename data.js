8970c797de53d79542618c821b5965b7737782fc
amaaninteriorsvly-code <amaaninteriorsvly@gmail.com>
2026-05-23 10:51:50 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,371 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-stair",
+      "name": "STAIR",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.16041534098977
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.3433525262544439,
+          "pitch": 0.07721185834807187,
+          "rotation": 0,
+          "target": "1-upper-living-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-upper-living-1",
+      "name": "UPPER LIVING 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.15947708514154613,
+          "pitch": 0.41241031649753523,
+          "rotation": 12.566370614359176,
+          "target": "2-upper-living-2"
+        },
+        {
+          "yaw": -3.102190363072358,
+          "pitch": 0.6066485860017625,
+          "rotation": 0,
+          "target": "0-stair"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-upper-living-2",
+      "name": "UPPER LIVING 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.919483654057089,
+          "pitch": 0.6885542650614234,
+          "rotation": 1.5707963267948966,
+          "target": "3-bedroom-4"
+        },
+        {
+          "yaw": -1.129446401338548,
+          "pitch": 0.6543552492836433,
+          "rotation": 5.497787143782138,
+          "target": "6-bedroom-5"
+        },
+        {
+          "yaw": -0.007814182063128783,
+          "pitch": 0.23845758742640477,
+          "rotation": 0,
+          "target": "1-upper-living-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-bedroom-4",
+      "name": "BEDROOM 4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7737931978556674,
+          "pitch": 0.45880465252423974,
+          "rotation": 4.71238898038469,
+          "target": "4-bedroom-41"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-bedroom-41",
+      "name": "BEDROOM 4.1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6661989659780936,
+          "pitch": 0.3374749331909541,
+          "rotation": 0,
+          "target": "5-bedroom-42"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-bedroom-42",
+      "name": "BEDROOM 4.2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5295823194017579,
+          "pitch": 1.007112654852893,
+          "rotation": 5.497787143782138,
+          "target": "2-upper-living-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-bedroom-5",
+      "name": "BEDROOM 5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8995792598734393,
+          "pitch": 0.37736855584162043,
+          "rotation": 1.5707963267948966,
+          "target": "7-bedroom-51"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-bedroom-51",
+      "name": "BEDROOM 5.1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.663447604759245,
+          "pitch": 0.32563942077129227,
+          "rotation": 0,
+          "target": "8-bedroom-52"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-bedroom-52",
+      "name": "BEDROOM 5.2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.258344126374757,
+          "pitch": 0.5737075802896516,
+          "rotation": 1.5707963267948966,
+          "target": "2-upper-living-2"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "SIDHEEQ FIRST FLOOR",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
